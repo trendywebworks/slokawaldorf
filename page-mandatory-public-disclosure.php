79c4d9fe3bab4f7112263b0a9ddcc858ad3960dd
trendywebworks <trendywebworks@gmail.com>
2026-05-21 19:23:38 +0530

    page added

---
+++ page-mandatory-public-disclosure.php
@@ -0,0 +1,8 @@
+<?php
+/* Template Name: Mandatory Public Disclosure */
+get_header();
+get_template_part( 'partials/mandatory-public-disclosure/_section', 'hero' );
+get_template_part( 'partials/mandatory-public-disclosure/_section', 'cbse-info' );
+get_template_part( 'partials/mandatory-public-disclosure/_section', 'join' );
+get_footer();
+?>
